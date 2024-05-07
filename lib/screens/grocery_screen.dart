a3e72651c088581286b1afb0fbacb894f6c5e39d
Ibrahim <ibrahimsakariyah@gmail.com>
2024-05-06 23:32:06 -0600

    gis update1

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fooderlich_scroll/screens/empty_grocery_screen.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -19,14 +20,39 @@ class GroceryScreen extends StatelessWidget {
           floatingActionButton: FloatingActionButton(
             onPressed: () {
               //TODO: Present GroceryItemScreen
+              final manager =
+                  Provider.of<GroceryManager>(context, listen: false);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => GroceryItemScreen(
+                            onCreate: (item) {
+                              manager.addItem(item);
+                              Navigator.pop(context);
+                            },
+                            onUpdate: (item) {
+                              //manager.updateItem(item, index);
+                            },
+                          )));
               // const EmptyGroceryScreen(); //not meant to be here
             },
             child: const Icon(Icons.add),
           ),
-          //body: buildGroceryScreen(),
+          body: buildGroceryScreen(),
         );
       }
     });
   }
+
   //TODO: add buildGrocerySCreen
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: ((context, manager, child) {
+      if(manager.groceryItems.isNotEmpty){ 
+        //TODO: add groceryListScreen
+      } else {
+        return const EmptyGroceryScreen();
+      }
+      
+    },),);
+  }
 }
